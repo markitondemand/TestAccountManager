236831813df162719a1a0d187a24c7574726eca4
Michael Leber <michael.leber@markit.com>
2017-02-10 11:37:44 -0700

    make Account struct initializer actually public

--- Source/Account.swift
+++ Source/Account.swift
@@ -6,8 +6,13 @@ import Foundation
 
 /// Simple struct that wraps account data
 public struct Account {
-    let userName: String
-    let password: String
+    public let userName: String
+    public let password: String
+    
+    public init(userName: String, password: String) {
+        self.userName = userName
+        self.password = password
+    }
 }
 
 
